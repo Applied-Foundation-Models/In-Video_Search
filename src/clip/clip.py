5dfa75283de2aeb5ab0e02d9c62bdb5da6f0ebf8
azza <azza.baatout@adesso.de>
2024-06-10 13:32:29 +0200

    added clip model class

--- src/clip/clip.py
+++ src/clip/clip.py
@@ -1,27 +1,33 @@
 import torch
 import clip
 
+
 class CLIP:
 
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model, self.preprocess = self.load_model()
+        self.model, self.preprocess = self.__load_model()
 
-    def load_model(self):
+    def __load_model(self) -> tuple:
         model, preprocess = clip.load("ViT-B/32", device=self.device)
         return model, preprocess
 
-    def generate_embeddings(self, image_dataset, texts, batch_size=32):
-        # process images
+    # load tokenizer
+    def tokenize_text(self, text_descriptions) -> torch.Tensor:
+        tokenized_text = clip.tokenize(text_descriptions).to(self.device)
+        return tokenized_text
+
+    def generate_dataset_embeddings(self, image_dataset, texts_descriptions) -> tuple:
         dataset_images_preprocessed = torch.cat([self.preprocess(image).unsqueeze(0) for image in image_dataset],
                                                 dim=0).to(
             self.device)
+        text_inputs = clip.tokenize(texts_descriptions).to(self.device)
+
         with torch.no_grad():
             dataset_image_embeddings = self.model.encode_image(dataset_images_preprocessed)
+            dataset_text_embeddings = self.model.encode_text(text_inputs)
 
-    def compute_cosine_similarity(self, embeddings_test, embeddings_validation):
-        similarity = torch.nn.functional.cosine_similarity(embeddings_test, embeddings_validation)
-        return similarity
+        return dataset_image_embeddings, dataset_text_embeddings
 
     def similarity_search(self, image_embeddings, text_embeddings, query_image, query_text):
         query_image = self.preprocess(query_image).unsqueeze(0).to(self.device)
