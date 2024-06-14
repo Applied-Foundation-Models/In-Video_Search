3188f6f54d40391dc6963977fad6bade745f6d6a
azzabaatout <81417054+azzabaatout@users.noreply.github.com>
2024-06-14 09:54:06 +0200

    Update pytesseract_image_to_text.py
    AFM-06: resolving conflice

--- src/ocr/pytesseract_image_to_text.py
+++ src/ocr/pytesseract_image_to_text.py
@@ -9,12 +9,10 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def extract_text_from_image(image_path):
+    # Define the base directory relative to this script
+    base_dir = os.path.dirname(os.path.abspath(__file__))
     full_image_path = os.path.join(base_dir, image_path)
     image = Image.open(full_image_path)
     text = pytesseract.image_to_string(image)
+    logger.info(f"Extracted text: {text}")
     return text
-
-
-# Specify the relative path to the image from the script's location
-relative_image_path = "test.png"
-logger.info(f"Extracted text: {extract_text_from_image(relative_image_path)}")
