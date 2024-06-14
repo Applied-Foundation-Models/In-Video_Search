ffa25701b5ff1676c2f48c2190459b1add212d51
azza <azza.baatout@adesso.de>
2024-06-14 09:48:50 +0200

    AFM-06: resolving conflict

--- src/ocr/pytesseract_image_to_text.py
+++ src/ocr/pytesseract_image_to_text.py
@@ -1,8 +1,23 @@
 import os
-import pytesseract
-from PIL import Image
-from loguru import logger
 
+import pytesseract
+from loguru import logger
+from PIL import Image
+
+# Define the base directory relative to this script
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def extract_text_from_image(image_path):
+    full_image_path = os.path.join(base_dir, image_path)
+    image = Image.open(full_image_path)
+    text = pytesseract.image_to_string(image)
+    return text
+
+
+# Specify the relative path to the image from the script's location
+relative_image_path = "test.png"
+logger.info(f"Extracted text: {extract_text_from_image(relative_image_path)}")
 
 def extract_text_from_image(image_path):
     # Define the base directory relative to this script
