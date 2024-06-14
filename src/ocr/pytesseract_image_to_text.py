65b48ebde572a73889f3a0c583247d63ffa93d98
azza <azza.baatout@adesso.de>
2024-06-14 09:53:26 +0200

    AFM-06: resolving conflict

--- src/ocr/pytesseract_image_to_text.py
+++ src/ocr/pytesseract_image_to_text.py
@@ -8,17 +8,6 @@ from PIL import Image
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def extract_text_from_image_deprecated(image_path):
-    full_image_path = os.path.join(base_dir, image_path)
-    image = Image.open(full_image_path)
-    text = pytesseract.image_to_string(image)
-    return text
-
-
-# Specify the relative path to the image from the script's location
-relative_image_path = "test.png"
-logger.info(f"Extracted text: {extract_text_from_image_deprecated(relative_image_path)}")
-
 def extract_text_from_image(image_path):
     # Define the base directory relative to this script
     base_dir = os.path.dirname(os.path.abspath(__file__))
