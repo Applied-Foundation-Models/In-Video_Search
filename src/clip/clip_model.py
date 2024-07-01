fd337e1ce2e1aa5e7effc894dca59fdc17897ca8
azza <azza.baatout@adesso.de>
2024-07-01 09:43:55 +0200

    Refactored the classes to avoid confusion

--- src/clip/clip_model.py
+++ src/clip/clip_model.py
@@ -1,7 +1,5 @@
 from src.clip.image_utils import generate_image_metadata, load_images_from_path
-from src.text_embedder.embedder import text_to_embedding_transformer
 from transformers import CLIPModel, CLIPProcessor
-from sentence_transformers import SentenceTransformer
 from loguru import logger
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -16,7 +14,6 @@ class CLIPEmbeddingsModel:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = CLIPModel.from_pretrained(model_name)
         self.processor = CLIPProcessor.from_pretrained(model_name)
-        self.text_embedder = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
         self.fig = plt.figure(figsize=(8, 20))
         self.dataset = None
         self.embeddings = None
@@ -43,11 +40,6 @@ class CLIPEmbeddingsModel:
         self.metadata = metadata
         logger.info(f"Metadata: {metadata}")
         return metadata
-
-    def generate_dataset_embeddings_standard_tokenizer(self, text):
-        model = self.text_embedder
-        query_text_embedding = text_to_embedding_transformer(text, model)
-        return query_text_embedding
 
     # store dataset in database
     def store_dataset_locally(self, metadata, embeddings):
@@ -86,12 +78,6 @@ class CLIPEmbeddingsModel:
         logger.info(f"Embeddings: {self.embeddings}")
         return outputs
 
-    def generate_dataset_embeddings_standard_tokenizer(self, text):
-        model = self.text_embedder
-        query_text_embedding = text_to_embedding_transformer(text, model)
-        self.text_embeddings = query_text_embedding
-        logger.info(f"Text embeddings with standard tokenizer: {self.text_embeddings}")
-
     def generate_image_embeddings(self, text_transcription, image):
         inputs = self.processor(
             text=text_transcription,
@@ -124,25 +110,25 @@ class CLIPEmbeddingsModel:
     # search for similar images in database (delete duplicates? or keep them?)
     def search_similar_images(self, query):
         # Get text embeddings of class dataset
-        text_embeddings = self.text_embeddings
+        text_embeddings = self.embeddings.text_embeds
 
         # Generate query text embeddings
-        # query_text_embedding = self.process_and_embedd_query_text(query)
-        model = self.text_embedder
-
-        query_text_embedding = text_to_embedding_transformer(query, model)
+        query_text_embedding = self.process_and_embedd_query_text(query)
 
         logger.info(f"Query text embedding shape: {query_text_embedding.shape}")
         logger.info(f"Text embeddings shape: {text_embeddings.shape}")
 
         # Compute cosine similarity between query text and all text embeddings
         similarities = cosine_similarity(query_text_embedding, text_embeddings, dim=1)
+        logger.info(f"Similarity scores: {similarities}")
 
         # Find the index of the maximum similarity score
         max_similarity_index = torch.argmax(similarities).item()
         max_similarity = similarities[max_similarity_index].item()
 
-        logger.info(f"Max similarity score: {max_similarity} at index: {max_similarity_index}")
+        logger.info(
+            f"Max similarity score: {max_similarity} at index: {max_similarity_index}"
+        )
 
         self.__display_similar_image(self.images[max_similarity_index])
 
@@ -171,81 +157,3 @@ class CLIPEmbeddingsModel:
         plt.imshow(image_array)
         plt.axis('off')  # Hide axis
         plt.show()
-
-    def search_similar_images_top_3(self, query, gt):
-        # text_embeddings = self.embeddings["text_embeds"]
-        text_embeddings = self.text_embeddings
-
-        # Generate query text embeddings
-        model = self.text_embedder
-
-        query_text_embedding = text_to_embedding_transformer(query, model)
-
-        logger.info(f"Query text embedding shape: {query_text_embedding.shape}")
-        logger.info(f"Text embeddings shape: {text_embeddings.shape}")
-
-        # Compute cosine similarity between query text and all text embeddings
-        similarities = cosine_similarity(query_text_embedding, text_embeddings, dim=1)
-
-        indices = torch.topk(similarities, 3)
-
-        logger.info(f"Top 3 Similarity scores: {indices} - GT is keyframe number {gt}")
-
-        logger.info(f"Length of img paths: {len(self.img_paths)}")
-
-        result = []
-
-        for i in range(3):
-            max_similarity_index = indices.indices[i].item()
-            # print len of img paths
-            if max_similarity_index <= len(self.img_paths):
-                logger.info(f"#####GT is keyframe number {gt}#####")
-                logger.info(
-                    f"Max similarity for index {max_similarity_index} is the keyframe {self.img_paths[max_similarity_index]}")
-                result.append(self.img_paths[max_similarity_index])
-                # Can display the image since paths are faulty 'magic-rabbit'
-                # opened_image = Image.open(self.img_paths[max_similarity_index])
-                # self.__display_similar_image(opened_image)
-            else:
-                logger.info(f"Index {max_similarity_index} is out of range")
-
-        return result
-
-    def search_similar_images_top_10(self, query, gt):
-        # text_embeddings = self.embeddings["text_embeds"]
-        text_embeddings = self.text_embeddings
-
-        # Generate query text embeddings
-        model = self.text_embedder
-
-        query_text_embedding = text_to_embedding_transformer(query, model)
-
-        logger.info(f"Query text embedding shape: {query_text_embedding.shape}")
-        logger.info(f"Text embeddings shape: {text_embeddings.shape}")
-
-        # Compute cosine similarity between query text and all text embeddings
-        similarities = cosine_similarity(query_text_embedding, text_embeddings, dim=1)
-
-        indices = torch.topk(similarities, 3)
-
-        logger.info(f"Top 3 Similarity scores: {indices} - GT is keyframe number {gt}")
-
-        logger.info(f"Length of img paths: {len(self.img_paths)}")
-
-        result = []
-
-        for i in range(10):
-            max_similarity_index = indices.indices[i].item()
-            # print len of img paths
-            if max_similarity_index <= len(self.img_paths):
-                logger.info(f"#####GT is keyframe number {gt}#####")
-                logger.info(
-                    f"Max similarity for index {max_similarity_index} is the keyframe {self.img_paths[max_similarity_index]}")
-                result.append(self.img_paths[max_similarity_index])
-                # Can display the image since paths are faulty 'magic-rabbit'
-                # opened_image = Image.open(self.img_paths[max_similarity_index])
-                # self.__display_similar_image(opened_image)
-            else:
-                logger.info(f"Index {max_similarity_index} is out of range")
-
-        return result
