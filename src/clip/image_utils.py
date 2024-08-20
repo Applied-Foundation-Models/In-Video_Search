7c2b0131afb9db5ed4b158047807ba893e168867
azza <azza.baatout@adesso.de>
2024-08-20 17:00:19 +0200

    AFM-10: deleted unused code

--- src/clip/image_utils.py
+++ src/clip/image_utils.py
@@ -1,15 +1,7 @@
 import os
 
-import requests
 from loguru import logger
 from PIL import Image
-
-
-def load_images_from_data(image_urls):
-    images = []
-    for url in image_urls:
-        images.append(Image.open(requests.get(url, stream=True).raw))
-    return images
 
 
 def load_images_from_path(image_paths):
